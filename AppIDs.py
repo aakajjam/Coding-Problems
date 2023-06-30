7a95516f5bb5007938cc7ddb38f30698159a1ff1
aakajjam <107234697+aakajjam@users.noreply.github.com>
2023-06-29 20:04:35 -0700

    Add files via upload

---
+++ AppIDs.py
@@ -0,0 +1,17 @@
+# This file is for AppID testing
+def AppID_testing(ticket_app_id = None, qe_test = None):
+    ticket_app_id = input("Enter the App ID shown in the TBM ticket: ")
+    qe_test = input("Enter the App ID shown on the phone: ")
+    result = ""
+    if ticket_app_id == qe_test:
+        return True, "These IDs match"
+    else:
+        for i, j in zip(ticket_app_id, qe_test):
+            if i == j:
+                result = result + j
+            else:
+                break
+        print("The content matches up to the stopping point", result)
+        return False, "These IDs do NOT match"
+
+print(AppID_testing())
